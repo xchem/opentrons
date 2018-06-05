9ca699edf820a6f61e453902cbaebb622a71c9f3
abradle <you@example.com>
2018-06-05 19:42:16 +0100

    Updated to display diffs

--- xchem_ot/tests/test_scripts/test_all_scripts.sh
+++ xchem_ot/tests/test_scripts/test_all_scripts.sh
@@ -4,6 +4,7 @@ DIFF=$(diff out.log examples/errors/file_error.out)
 if [ "$DIFF" != "" ]
 then
     echo Difference between file_error
+    echo $DIFF
     exit 1
 fi
 xchem_ot --yaml_path examples/errors/header_error.yaml > out.log
@@ -11,6 +12,7 @@ DIFF=$(diff out.log examples/errors/header_error.out)
 if [ "$DIFF" != "" ]
 then
     echo Difference between header_error
+    echo $DIFF
     exit 1
 fi
 rm -r acylation_*
@@ -19,6 +21,7 @@ DIFF=$(diff out.log examples/errors/inherit_error.out)
 if [ "$DIFF" != "" ]
 then
     echo Difference between inherit_error
+    echo $DIFF
     exit 1
 fi
 xchem_ot --yaml_path examples/errors/solvent_error.yaml > out.log
@@ -26,6 +29,7 @@ DIFF=$(diff out.log examples/errors/solvent_error.out)
 if [ "$DIFF" != "" ]
 then
     echo Difference between solvent_error
+    echo $DIFF
     exit 1
 fi
 rm -r acylation_*
@@ -34,5 +38,6 @@ DIFF=$(diff out.log examples/errors/trough_error.out)
 if [ "$DIFF" != "" ]
 then
     echo Difference between trough_error
+    echo $DIFF
     exit 1
 fi
